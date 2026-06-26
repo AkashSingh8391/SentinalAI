dd297a07d98b34594119ea3cd2793c47a9af70d2
Akash Singh <akashgurjar6266@gmail.com>
2026-06-26 23:08:17 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/SentinalAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinalAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinalAiApplication.class, args);
+	}
+
+}
