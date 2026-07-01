522263d2b586ed33ef3c1cd497dfa3d1051c5752
Akash Singh <akashgurjar6266@gmail.com>
2026-07-01 22:45:14 +0530

    Create entity for Users

---
+++ src/main/java/com/safety/model/User.java
@@ -0,0 +1,27 @@
+package com.safety.model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    public User() {}
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
